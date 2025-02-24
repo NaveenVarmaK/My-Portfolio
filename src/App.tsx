0d94a0e6c4904baedd0f06558e4ce61026236eb5
Naveen Varma Kalidindi <64104378+NaveenVarmaK@users.noreply.github.com>
2025-02-24 17:35:20 +0100

    Testing Sample Structured Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,41 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+
 import { useEffect } from "react";
 
 function App() {
@@ -7,12 +45,12 @@ function App() {
     script.text = JSON.stringify({
       "@context": "https://schema.org",
       "@type": "Person",
-      "name": "Your Name",
-      "jobTitle": "Your Job Title",
-      "url": "https://your-portfolio.com",
+      "name": "Naveen Varma KALIDINDI",
+      "jobTitle": "Computer Science Student",
+      "url": "https://naveenvarmak.github.io/My-Portfolio/",
       "sameAs": [
-        "https://www.linkedin.com/in/yourprofile",
-        "https://github.com/yourusername"
+        "https://www.linkedin.com/in/knaveenv",
+        "https://github.com/NaveenVarmaK"
       ]
     });
     document.head.appendChild(script);
@@ -20,7 +58,7 @@ function App() {
 
   return (
     <div>
-      <h1>Welcome to My Portfolio</h1>
+      <h1>Welcome to My Portfolioooooooo</h1>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <h1>
+      This is Main
+    </h1>
     <App />
   </StrictMode>,
 )
